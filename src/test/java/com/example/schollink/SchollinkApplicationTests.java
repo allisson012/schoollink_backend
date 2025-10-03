d17bbae9c10576f288414a41a42101b34c2cfd50
allisson012 <allissoncastilho@gmail.com>
2025-10-03 13:56:00 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/test/java/com/example/schollink/SchollinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schollink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchollinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
