04c705b3fcab217dc8e54ec2edfe5ecc0d3eab4f
Pedro <pedro.fialho000@gmail.com>
2025-10-07 22:23:44 -0300

    refactor: mudando response da autenticação de usuario

--- src/main/java/com/example/schollink/controller/AuthController.java
+++ src/main/java/com/example/schollink/controller/AuthController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.schollink.model.User;
 import com.example.schollink.service.AuthService;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
     @Autowired
     private AuthService authService;
 
@@ -25,12 +25,17 @@ public class AuthController {
     public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, String> loginRequest) {
         String email = loginRequest.get("email");
         String password = loginRequest.get("password");
-        boolean autenticado = authService.autenticarUsuario(email, password);
+
+        User usuario = authService.autenticarUsuario(email, password);
+
         Map<String, String> response = new HashMap<>();
-        if (autenticado) {
+        if (usuario != null) {
             response.put("message", "Login bem sucedido");
+            response.put("id", String.valueOf(usuario.getId()));
+            response.put("nome", usuario.getNome());
             return ResponseEntity.ok(response);
         }
+
         response.put("message", "email ou senha inválidos");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
