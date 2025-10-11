96112f23c4a39403ba8af8bd3852022379e456c7
allisson012 <allissoncastilho@gmail.com>
2025-10-11 18:15:54 -0300

    Criando os model de disciplina,endereço e criando relação entre os models

---
+++ src/main/java/com/example/schollink/model/Disciplina.java
@@ -0,0 +1,45 @@
+package com.example.schollink.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Disciplina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "professorId", referencedColumnName = "id")
+    private Professor professor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+}
--- src/main/java/com/example/schollink/model/Professor.java
+++ src/main/java/com/example/schollink/model/Professor.java
@@ -1,19 +1,116 @@
 package com.example.schollink.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
 public class Professor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idProfessor;
+    private Long id;
 
     @OneToOne
     @JoinColumn(name = "idUser", referencedColumnName = "id")
     private User user;
+
+    private LocalDate dataContratacao;
+    private String formacaoAcademica;
+    @OneToMany(mappedBy = "professor")
+    private List<Disciplina> disciplinas = new ArrayList<>();
+    // disciplinas que leciona
+    // turmas estou pensando em colocar todas turmas que o professor ministra para o
+    // acesso a elas ficar mais simples
+    private String registroProfissional;
+    private double cargaHorariaSem;
+    @Enumerated(EnumType.STRING)
+    private Turno turno;
+    private double salario;
+
+    // historico de aulas os dois teria que ter classe separadas para guardar essas
+    // informações
+    // horario das aulas
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public void setDataContratacao(LocalDate dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+
+    public String getFormacaoAcademica() {
+        return formacaoAcademica;
+    }
+
+    public void setFormacaoAcademica(String formacaoAcademica) {
+        this.formacaoAcademica = formacaoAcademica;
+    }
+
+    public String getRegistroProfissional() {
+        return registroProfissional;
+    }
+
+    public void setRegistroProfissional(String registroProfissional) {
+        this.registroProfissional = registroProfissional;
+    }
+
+    public double getCargaHorariaSem() {
+        return cargaHorariaSem;
+    }
+
+    public void setCargaHorariaSem(double cargaHorariaSem) {
+        this.cargaHorariaSem = cargaHorariaSem;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(List<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
 }
