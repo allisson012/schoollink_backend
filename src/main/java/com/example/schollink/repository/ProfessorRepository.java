12367a62823e92f535158efa774b3695f60b1bb7
bd1ogo <barrosof.diogo@gmail.com>
2025-10-14 11:56:25 -0300

    criação completa do módulo Professor (CRUD e Integrações)

---
+++ src/main/java/com/example/schollink/repository/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.example.schollink.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.schollink.model.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
