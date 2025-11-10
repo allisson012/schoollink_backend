16a5de69a7320420e4c91ea020089812f85c2fee
Pedro <pedro.fialho000@gmail.com>
2025-11-10 16:53:21 -0300

    refactor: adicionando campo no DTO

--- src/main/java/com/example/schollink/Dto/AlunoParaTurmaDto.java
+++ src/main/java/com/example/schollink/Dto/AlunoParaTurmaDto.java
@@ -3,6 +3,7 @@ package com.example.schollink.Dto;
 public class AlunoParaTurmaDto {
     private Long id;
     private String nome;
+    private Boolean turma;
     public Long getId() {
         return id;
     }
@@ -15,9 +16,16 @@ public class AlunoParaTurmaDto {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public AlunoParaTurmaDto(Long id, String nome) {
+    public AlunoParaTurmaDto(Long id, String nome, Boolean turma) {
         this.id = id;
         this.nome = nome;
+        this.turma = turma;
     }
-    
+    public Boolean getTurma() {
+        return turma;
+    }
+    public void setTurma(Boolean turma) {
+        this.turma = turma;
+    }
+
 }
